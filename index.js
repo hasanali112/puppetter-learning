0e0cd02e36e058caee0329e21af476e3f13832a6
hasanali112 <sohagali.ru.ac@gmail.com>
2025-03-31 23:51:11 +0600

    learn: title, eval, pdf

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ const puppeteer = require("puppeteer");
 
     const page = await browser.newPage();
     await page.goto("https://google.com");
-    await page.screenshot({ path: "google.png" });
+
+    const title = await page.title();
+    console.log(title);
+
+    const heading = await page.$eval("p", (elements) => {
+      return elements.map((element) => element.textContent);
+    });
+    console.log(heading);
+
+    await page.pdf({ path: "google.pdf", format: "A4" });
+
     await browser.close();
   } catch (error) {
     console.log(error);
